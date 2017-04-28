6eb823ecd77789bd3a16419c61cad503bf716f60
Conner Simmering <connersimmering@gmail.com>
2017-04-27 23:34:24 -0600

    maven-init

---
+++ src/main/java/com/simmeringc/websitePoller/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Conner on 4/28/17.
+ */
+
+package com.simmeringc.websitePoller;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
